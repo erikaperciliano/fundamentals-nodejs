71395d0cd298aa225af2247630ad9956e542052b
Erika Perciliano <erikaperciliano.developer@gmail.com>
2023-10-30 18:18:24 +0000

    starts methods

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
+    const { method, url } = req;
+
+    if(method === 'GET' && url === '/users'){
+        return res.end('User list!')
+    }
+
+    if(method === 'POST' && url === '/users'){
+        return res.end('User creation!')
+    }
     return res.end('Hello World!')
 });
 
