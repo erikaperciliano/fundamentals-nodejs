8f2582e2a3a8f6093329a8620a3678912f976945
Erika Perciliano <erikaperciliano.developer@gmail.com>
2023-11-01 16:54:27 +0000

    add HTTP Status codes

--- src/server.js
+++ src/server.js
@@ -19,9 +19,9 @@ const server = http.createServer((req, res) => {
             email:'johndoe@example.com'
         })
 
-        return res.end('User creation!')
+        return res.writeHead(201).end();
     }
-    return res.end('Hello World!')
+    return res.writeHead(404).end()
 });
 
-server.listen(3333)
+server.listen(3333);
