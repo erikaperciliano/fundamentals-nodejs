ec7c8f5e43bb20fdd77fd202bbf3bed150f299a0
Erika Perciliano <erikaperciliano.developer@gmail.com>
2023-11-01 17:39:29 +0000

    creates read streams

---
+++ streams/fundamentals.js
@@ -0,0 +1,22 @@
+import { Readable } from 'nodfe:stream';
+
+class OneToHundredStream extends Readable {
+    index = 1;
+
+    _read(){
+        const i = this.index ++;
+
+        setTimeout(() => {
+            if(i > 100){
+                this.push(null);
+            }else {
+                const buf = Buffer.from(String(1));
+    
+                this.push(buf);
+            }
+        }, 100)
+    }
+}
+
+new OneToHundredStream()
+    .pipe(process.stdout)
